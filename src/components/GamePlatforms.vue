12b77c6186ce8144b2d3da49f1c6d39d6505e226
Dmitriy Konoplev <snowbooloff163@gmail.com>
2023-01-26 16:38:26 +0300

    Bug fixes and reworked icons

--- src/components/GamePlatforms.vue
+++ src/components/GamePlatforms.vue
@@ -1,27 +1,33 @@
 <script setup>
-const prop = defineProps(['platforms'])
+const props = defineProps({
+  platforms: {
+    type: Array,
+    required: true
+  }
+})
+
+const validPlatforms = ['PC', 'PlayStation', 'Xbox', 'Android', 'iOS', 'Nintendo']
+const filteredPlatforms = props.platforms.filter((platform) =>
+  validPlatforms.includes(platform['platform']['name'])
+)
 </script>
 
 <template>
-  <div class="game-platforms">
-    <span>
-      <c-icon
-        class="game-platforms__icon"
-        v-for="platform of platforms"
-        :icon="platform['platform']['name']"
-        :key="platform['platform']['name']"
-      />
-    </span>
+  <div class="game-platforms flex flex-align-center">
+    <component
+      v-for="platform of filteredPlatforms"
+      :key="platform['platform']['name']"
+      :is="`icon-${platform['platform']['name'].toLowerCase()}`"
+      class="game-platforms__icon main-white"
+    />
   </div>
 </template>
 
 <style scoped>
+.game-platforms {
+  gap: var(--medium-spacing);
+}
 .game-platforms__icon {
   height: 100%;
-  margin-right: var(--medium-spacing);
-}
-
-.game-platforms__icon:nth-last-child(1) {
-  margin-right: 0;
 }
 </style>
