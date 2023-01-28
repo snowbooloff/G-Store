16dc8996591122209da95fb66b4d9d39764a5102
Dmitriy Konoplev <snowbooloff163@gmail.com>
2023-01-28 15:24:07 +0300

    page loader fix

--- src/components/PageLoader.vue
+++ src/components/PageLoader.vue
@@ -8,7 +8,7 @@ export default {
 
 <template>
   <div class="loading flex flex-justify-center flex-align-center">
-    <div class="loading__spiner"></div>
+    <icon-logo class="loading__spiner main-white" />
   </div>
 </template>
 
@@ -19,17 +19,13 @@ export default {
   position: fixed;
   top: 0;
   left: 0;
-  z-index: 160;
+  z-index: 140;
   background-color: var(--translucent-black);
 }
 
 .loading__spiner {
-  height: 150px;
-  width: 150px;
-  /* mask-image: url('@/assets/icons/logo.svg'); */
-  mask-size: contain;
-  mask-repeat: no-repeat;
-  background-color: var(--main-white);
+  height: 15%;
+  aspect-ratio: 1 / 1;
   animation: loading 1.25s linear infinite;
 }
 
