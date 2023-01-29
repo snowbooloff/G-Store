df6c888fc9110797af7eefc0e8810dc8492c731c
Dmitriy Konoplev <snowbooloff163@gmail.com>
2023-01-30 01:11:04 +0300

    created firebaseConfig file

--- src/main.js
+++ src/main.js
@@ -12,18 +12,11 @@ import ComponentsIcons from './components/icons'
 //Directives
 import Intersection from './directives/Vintersection'
 
+//Firebase
 import { initializeApp } from 'firebase/app'
 import { getDatabase } from 'firebase/database'
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyDraHn-WZ632cl6lPtJlsinpvEPj4qIxxo',
-  authDomain: 'gstore-buy-4d273.firebaseapp.com',
-  projectId: 'gstore-buy-4d273',
-  storageBucket: 'gstore-buy-4d273.appspot.com',
-  messagingSenderId: '658773696495',
-  appId: '1:658773696495:web:4a63ff491da071945be01a',
-  databaseURL: 'https://gstore-buy-4d273-default-rtdb.europe-west1.firebasedatabase.app/'
-}
+import { firebaseConfig } from './firebaseConfig'
 
 const FireBaseApp = initializeApp(firebaseConfig)
 const database = getDatabase(FireBaseApp)
