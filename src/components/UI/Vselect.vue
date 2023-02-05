ce31159243a2a96f51cf8d67572ec8deef9263e1
Dmitriy Konoplev <snowbooloff163@gmail.com>
2023-02-06 01:57:46 +0300

    Обновлен компонент селекта на ts

--- src/components/UI/Vselect.vue
+++ src/components/UI/Vselect.vue
@@ -1,26 +1,33 @@
-<script setup>
-const props = defineProps({
-  modelValue: {
-    type: String,
-    required: true
-  },
-  options: {
-    type: Array,
-    required: true
-  }
-})
+<script setup lang="ts">
+const props = defineProps<{
+  modelValue: string
+  options: [{ value: string; name: string }]
+}>()
+
+const emit = defineEmits(['update:modelValue'])
+
+function updateValue(Event: Event) {
+  emit('update:modelValue', (Event.target as HTMLInputElement).value)
+}
 </script>
 
 <template>
   <select
     class="select main-white"
     :value="modelValue"
-    @change="$emit('update:modelValue', $event.target.value)"
+    @change="updateValue"
   >
-    <option class="second-white" value="">
+    <option
+      class="second-white"
+      value=""
+    >
       <slot></slot>
     </option>
-    <option v-for="option in options" :key="option.value" :value="option.value">
+    <option
+      v-for="option in options"
+      :key="option.value"
+      :value="option.value"
+    >
       {{ option.name }}
     </option>
   </select>
