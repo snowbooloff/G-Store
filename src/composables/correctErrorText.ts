218ea85f374452b8075f7a173088cc074f2c425e
Dmitriy Konoplev <snowbooloff163@gmail.com>
2023-02-07 08:02:08 +0300

    Добвлен composable преобразования ошибок

---
+++ src/composables/correctErrorText.ts
@@ -0,0 +1,12 @@
+import type { Ref } from 'vue'
+
+export default function correctErrorText(error: any, placeholder: Ref): void {
+  const regexp = new RegExp(/[^\x2F][a-z,-]+(?=\x29)/g)
+
+  let errorMessage = error.message.match(regexp)[0]
+
+  errorMessage = errorMessage.replace(/-/g, ' ')
+  errorMessage = errorMessage.replace(errorMessage[0], errorMessage[0].toUpperCase())
+
+  placeholder.value = errorMessage
+}
