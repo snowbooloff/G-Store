8d0a545d5fdc1167913ed7aa600fe114af158be3
Dmitriy Konoplev <snowbooloff163@gmail.com>
2023-02-07 08:03:10 +0300

    Переделаны стили для auth страниц

---
+++ src/views/authViews/LoginView.vue
@@ -0,0 +1,75 @@
+<script setup lang="ts">
+import { ref, reactive } from 'vue'
+
+//Composables
+import correctErrorText from '../../composables/correctErrorText'
+
+//Utils
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+
+const userData = reactive({
+  email: '',
+  password: ''
+})
+
+function loginingUser() {
+  const auth = getAuth()
+  signInWithEmailAndPassword(auth, userData.email, userData.password).catch((error) => {
+    correctErrorText(error, loginError)
+    setTimeout(() => (loginError.value = ''), 1000)
+  })
+}
+
+const loginError = ref('')
+</script>
+
+<template>
+  <section class="page-block flex flex_column flex_align-center">
+    <p class="page-block__title second-white">SIGN IN TO YOUR GSTORE ACCOUNT</p>
+
+    <form class="auth-form flex flex flex_column">
+      <div class="auth-area">
+        <label class="auth-area__label second-white">Email Address:</label>
+        <v-input
+          class="auth-area__input"
+          v-model="userData.email"
+          type="email"
+        />
+      </div>
+
+      <div class="auth-area">
+        <label class="auth-area__label second-white">Password:</label>
+        <v-password-input
+          class="auth-area__input"
+          v-model="userData.password"
+          maxlength="12"
+        />
+      </div>
+      <p
+        class="auth-form__error"
+        v-show="loginError.length"
+      >
+        {{ loginError }}
+      </p>
+      <button
+        class="auth-form__bttn bttn bttn_buy"
+        :class="{ shake: loginError.length }"
+        @click="loginingUser"
+      >
+        SIGN IN
+      </button>
+      <span class="auth-form__span second-white">
+        New to GSTORE?
+        <router-link
+          to="/register"
+          class="main-white"
+          >Sign Up</router-link
+        >
+      </span>
+    </form>
+  </section>
+</template>
+
+<style scoped>
+@import url('./auth.css');
+</style>
