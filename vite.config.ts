9441ad7d36a4a75120fe85af18bdbda424dba7fd
Dmitriy Konoplev <snowbooloff163@gmail.com>
2023-02-11 07:48:01 +0300

    Возможность писать элиасы в импортах

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
-    alias: [
-      {
-        find: /^~(.*)$/,
-        replacement: 'node_modules/$1'
-      }
-    ]
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
   }
 })
