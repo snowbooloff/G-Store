5db814b4705f2339875b2d2af3b244e9ced7d672
Dmitriy Konoplev <snowbooloff163@gmail.com>
2023-02-13 04:04:29 +0300

    Добавлен rollup для дробления бандла на чанки

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,14 @@ const path = require('path')
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/G-Store/',
+  build: {
+    outDir: './dist/',
+    rollupOptions: {
+      output: {
+        sanitizeFileName: false
+      }
+    }
+  },
   plugins: [vue()],
   resolve: {
     alias: {
