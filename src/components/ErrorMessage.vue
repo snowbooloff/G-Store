b85c40c2fe1610217ebd12667b10dd7c4a0ca3ca
Dmitriy Konoplev <snowbooloff163@gmail.com>
2023-02-20 02:02:42 +0300

    Добавлен компанет error message для отбражения ошибок при регистрации/логине

---
+++ src/components/ErrorMessage.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { watch } from 'vue'
+const props = defineProps<{
+  errorText: string
+  delay: number
+}>()
+
+const emit = defineEmits(['update:errorText'])
+
+watch(props, (newValue) => {
+  if (newValue.errorText) {
+    setTimeout(() => emit('update:errorText', ''), 1500)
+  }
+})
+</script>
+
+<template>
+  <p
+    class="error-info"
+    v-show="errorText.length"
+  >
+    {{ errorText }}
+  </p>
+</template>
+
+<style scoped>
+.error-info {
+  text-align: center;
+  color: #d00000;
+}
+</style>
